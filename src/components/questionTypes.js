3bfd813da4cef87e0235a7b54f11d45c822cf56c
karishni <karishni25@gmail.com>
2020-10-23 09:33:04 +0530

    added question type selector and question array

---
+++ src/components/questionTypes.js
@@ -0,0 +1,16 @@
+const QuestionTypes=[
+    {
+        value: "singleLine",
+        label: "Single Line Answer"
+    },
+    {
+        value: "multipleLine",
+        label: "Multiple Line Answer"
+    },
+    {
+        value: "radioButtons",
+        label: "Single correct multiple options"
+    }
+]
+
+export default QuestionTypes;
